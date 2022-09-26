58b1aa1e7b283732162b7c7383b2d3a5749fc158
Monstarules <orionsmonster@yahoo.com>
2022-09-25 21:23:39 -0400

    Revert abilities stuff so we can avoid the perils of yeehaw cowboy code

--- gfx/pokemon/bitmask_johto.asm
+++ gfx/pokemon/bitmask_johto.asm
@@ -50,6 +50,7 @@ JohtoBitmaskPointers:
 	dw MurkrowBitmasks
 	dw SlowkingBitmasks
 	dw MisdreavusBitmasks
+	dw UnownBitmasks
 	dw WobbuffetBitmasks
 	dw GirafarigBitmasks
 	dw PinecoBitmasks
@@ -151,6 +152,7 @@ UmbreonBitmasks:    INCLUDE "gfx/pokemon/johto/umbreon/bitmask.asm"
 MurkrowBitmasks:    INCLUDE "gfx/pokemon/johto/murkrow/bitmask.asm"
 SlowkingBitmasks:   INCLUDE "gfx/pokemon/johto/slowking/bitmask.asm"
 MisdreavusBitmasks: INCLUDE "gfx/pokemon/johto/misdreavus/bitmask.asm"
+UnownBitmasks:      INCLUDE "gfx/pokemon/johto/unown/bitmask.asm"
 WobbuffetBitmasks:  INCLUDE "gfx/pokemon/johto/wobbuffet/bitmask.asm"
 GirafarigBitmasks:  INCLUDE "gfx/pokemon/johto/girafarig/bitmask.asm"
 PinecoBitmasks:     INCLUDE "gfx/pokemon/johto/pineco/bitmask.asm"
